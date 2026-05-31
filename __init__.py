4e1cab6c584ac318bd682ba10a2dd4a313fad4c3
Wali <wali25560@iiitd.ac.in>
2026-05-31 11:42:26 +0530

    comments added in __init.py and api.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,9 @@
-from graph import graph
+from graph import graph  
 
 __all__ = ["graph"]
+
+#What's it used for ?
+
+#Without __init__.py Flight_agent is just a folder and whenever u type -> "from Flight_agent import graph" it wont't work because
+#it's not a package it's just a folder. this file basically converts this folder into a package from where Python can "import" modules from.
+#think of it like a car with all the parts but have no chassis plate which makes it legally a car without which its just smth sitting in a garage.
--- api.py
+++ api.py
@@ -1,8 +1,11 @@
+#Giving the agent a new home from terminal to a web server so that instead of putting ur prompts and running them in terminal a user can basically 
+#run them from anywhere on the web. The agents u see on the web are basically ai architecture wrapped up in http endpoints and presented beautifully in a frontend ui .
+
 from dotenv import load_dotenv
 load_dotenv()
-__import__('pysqlite3')
+__import__('pysqlite3')   #chromodb uses sqlite but render linux servers operate on an older version of sqlite which wont work so pysqlite-3 is used so that chromadb can use that instead of the old sqlite
 import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')  #tricking chromadb to use pysqlite3 instead of sqlite3 by renaming it
 from fastapi import FastAPI
 from pydantic import BaseModel
 from langchain_core.messages import HumanMessage
@@ -12,8 +15,9 @@ app = FastAPI()
 from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["https://luxury-youtiao-5bef59.netlify.app"],   # lock this down to your frontend URL in production
+    CORSMiddleware,  #javascript on one domain cannot talk to the server of another domain (same origin poilicy)
+                     #without course middleware our api on render cannot talk to the frontend on netlify
+    allow_origins=["https://luxury-youtiao-5bef59.netlify.app"],   # lock this down to the frontend URL in production
     allow_methods=["*"],
     allow_headers=["*"],
 )
